ae3274be23f07bd100d53c317aa62c0f039fa084
kis3689 <kimin6553@gmail.com>
2020-05-29 17:00:50 +0900

    ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-ngx-translate';
+
+  private translate:TranslateService
+
+  constructor(translate: TranslateService) {
+    this.translate = translate
+  }
+
+  public ngOnInit():void {
+    this.translate.setDefaultLang('en')
+  }
+
+  public switchLanguage(lang: string):void {
+    this.translate.use(lang);
+  }
 }
